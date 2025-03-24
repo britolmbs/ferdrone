d7130d135f8103fd4fff6174806663497adf915e
Lucas Brito <britolmbs@hotmail.com>
2025-03-24 20:29:52 -0300

    feat: implementar carrossel de Trabalhos usando Embla Carousel

--- src/app/components/trabalhos.tsx
+++ src/app/components/trabalhos.tsx
@@ -1,5 +1,6 @@
 "use client"
 import useEmblaCarousel from "embla-carousel-react"
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const trabalhos = [
     {
@@ -28,18 +29,35 @@ export function Trabalhos() {
     }
 
     return(
-       <section className="bg-[#FFD449] py-16">
+       <section className="bg-[#E51310] py-16">
         <div className="container mx-auto px-4">
             <h2 className="text-4xl text-center font-bold mb-12">Trabalhos Realizados</h2>
+            <div className="relative max-w-4xl mx-auto">
             <div className="overflow-hidden" ref={emblaRef}>
                 <div className="flex">
                     {trabalhos.map((item, index) => (
                         <div key={index} className="flex-[0_0_100%] min-w-0 px-3">
+                            <article className="bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col">
+                            <div className=" flex flex-col items-center text-center space-y-4">
+                                <div className="relative w-24 h-24">
+
+                                </div>
+                            </div>
+                            </article>
                             
                         </div>
                     ))}
                 </div>
-            </div>
+                </div>
+                <button onClick={scrollPrev} className="bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10">
+                <ChevronLeft className="w-6 h-6 text-gray-600" />
+                </button>
+                <button onClick={scrollNext} className="bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10">
+                    <ChevronRight className="w-6 h-6 text-gray-600" />
+                </button>
+
+
+         </div>   
         </div>
        </section>
     )
