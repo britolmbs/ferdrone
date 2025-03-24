31ef8a3ce00fb70c31cf8fa5e7a5fe846830f57d
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-03-24 11:52:51 -0300

    Iniciando pagina de trabalho

---
+++ src/app/components/trabalhos.tsx
@@ -0,0 +1,29 @@
+"use client"
+import useEmblaCarousel from "embla-carousel-react"
+
+const trabalhos = [
+    {
+        title:'',
+        local: '',
+        video: '',
+
+    },
+    {
+        title: '',
+        local: '',
+        video: '',
+    },
+]
+
+export function Trabalhos(){
+
+    function scrollPrev () {
+        emblaApi?:
+    }
+
+    return(
+        <div>
+            <h1>Trabalhos em Videos</h1>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/footer"
 import { Hero } from "./components/hero"
 import { About } from "./components/about";
 import { Services } from "./components/services";
+import { Trabalhos } from "./components/trabalhos";
 
 
 
@@ -15,6 +16,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <Trabalhos />
       <Footer />
     </div>
   );
