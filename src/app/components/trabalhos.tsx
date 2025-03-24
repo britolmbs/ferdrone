a953c3788a6387c6d9d7c05943bdebee023eed38
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-03-24 12:57:57 -0300

    Trabalho

--- src/app/components/trabalhos.tsx
+++ src/app/components/trabalhos.tsx
@@ -15,15 +15,32 @@ const trabalhos = [
     },
 ]
 
-export function Trabalhos(){
+export function Trabalhos() {
+    const [emblaRef, emblaApi] = useEmblaCarousel({
+        loop: true
+    })
 
     function scrollPrev () {
-        emblaApi?:
+        emblaApi?.scrollPrev();
+    }
+    function scrollNext () {
+        emblaApi?.scrollNext();
     }
 
     return(
-        <div>
-            <h1>Trabalhos em Videos</h1>
+       <section className="bg-[#FFD449] py-16">
+        <div className="container mx-auto px-4">
+            <h2 className="text-4xl text-center font-bold mb-12">Trabalhos Realizados</h2>
+            <div className="overflow-hidden" ref={emblaRef}>
+                <div className="flex">
+                    {trabalhos.map((item, index) => (
+                        <div key={index} className="flex-[0_0_100%] min-w-0 px-3">
+                            
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
+       </section>
     )
 }
