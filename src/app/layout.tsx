50f9aa5d2837374a28de7bd9a4adcedb5b8b97e5
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-04-02 09:49:01 -0300

    corrigindo import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {AosInit} from './aos-init'
+import {AosInit} from '../app/components/aos-init'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
